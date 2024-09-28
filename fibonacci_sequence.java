3e68166182afaa3a0792ee67e63ad493e1808da2
FelipeCardozo0 <focardo@emory.edu>
2024-09-27 23:42:10 -1100

    Create fibonacci_sequence.java

---
+++ fibonacci_sequence.java
@@ -0,0 +1,22 @@
+public class fibonacci_sequence {
+    public static void main(String[] args) {
+        System.out.println((fibonacci_sequence(10)));
+    }
+
+    public static int fibonacci_sequence(int fn) {
+        int f0=0;
+        int f1=1;
+        // int fn = fn-1+fn-2
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(fn);
+            }
+        }
+    }
+}
+
+// 0,
+// 0, n1+0,n1+0 , n1+0+n1+0, n1+0 + n1+0+n1+0_, n1+0+n1+0+ n1+0 + n1+0+n1+0_,
+
+// first loop = runs untill y reaches n
+// 2nd loop =  runs the sequence
